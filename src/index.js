e84677e56be5d6ece7f2e64b9ebf05c2b97fdd30
Alex Scott <codetheorist@gmail.com>
2017-09-12 15:20:50 +0000

    Repo init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+const App = () => <div><h1>Perfectly simple Webpack 2 setup</h1><p>WOW, it works!</p></div>;
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
